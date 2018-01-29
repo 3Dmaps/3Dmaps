2b8d341bb72790f2815bc702a4db6928ebe0eef3
ollivaan <ollipekka.vaananen@helsinki.fi>
2018-01-29 16:54:27 +0200

    Zoom buttons and script

---
+++ Assets/Scripts/UI/Zoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///<summary>
+/// This class changes the scale of target object by the multiplier  
+/// </summary>
+public class Zoom : MonoBehaviour {
+	public GameObject target;
+	public float multiplier = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ZoomMap() {
+		Vector3 scale = target.transform.localScale;
+		scale = scale * multiplier;
+		target.transform.localScale = scale;
+
+
+	}
+}
