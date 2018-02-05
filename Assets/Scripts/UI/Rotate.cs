a53b062be45ac0365ed0e718742f56eebb077af1
Op <ollipekka.vaananen@helsinki.fi>
2018-02-05 16:52:55 +0200

    Rotation buttons and tests

---
+++ Assets/Scripts/UI/Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///<summary>
+/// This class rotate target object by the turningSpeed  
+/// </summary>
+
+public class Rotate : MonoBehaviour {
+	public GameObject target;
+	public bool isPressed = false;
+    public float turningSpeed = 30f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPressed) {
+			RotateTarget (turningSpeed * Time.deltaTime);
+			print ("jee");
+		}
+	
+	}
+
+	public void RotateTarget(float amount) {
+		target.transform.Rotate(Vector3.up, amount);
+
+	}
+	///<summary>
+	/// EventTrigger calls onPointerDown and OnPointerUp  
+	/// </summary>
+	public void OnPointerDown() {
+		isPressed = true;
+	}
+	public void OnPointerUp() {
+		isPressed = false;
+	}
+}
