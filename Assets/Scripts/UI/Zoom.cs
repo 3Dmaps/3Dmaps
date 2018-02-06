7c74d535a03473d46a9b236a60430200d73e3bb7
jurintal1 <jussi.rintala@gmail.com>
2018-02-06 16:39:25 +0200

    limiting the number of one-way zoom operations

--- Assets/Scripts/UI/Zoom.cs
+++ Assets/Scripts/UI/Zoom.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class Zoom : MonoBehaviour {
 	public GameObject target;
 	public float multiplier = 0.8f;
+    public static int zoomLevel = 0;
+    public int zoomLimit = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +21,25 @@ public class Zoom : MonoBehaviour {
 	}
 
 	public void ZoomTarget() {
-		Vector3 scale = target.transform.localScale;
-		scale = scale * multiplier;
-		target.transform.localScale = scale;
+        if (zoomingAllowed()) {
+            Vector3 scale = target.transform.localScale;
+            scale = scale * multiplier;
+            target.transform.localScale = scale;
+        }        
+     }
 
-
-	}
+    private bool zoomingAllowed() {
+        if (multiplier < 1 && (zoomLevel + zoomLimit) > 0)
+        {
+            zoomLevel--;
+            return true;
+        }
+        if (multiplier > 1 && zoomLevel < zoomLimit)
+        {
+            zoomLevel++;
+            return true;
+        }
+        return false;
+    }
+    
 }
