5fc621031d78ca757d699618bbf75fed818f8450
ollisami <ollisami@cs.helsinki.fi>
2018-02-07 10:54:50 +0200

    Fixed some logic issues with zoom limits.

--- Assets/Scripts/UI/Zoom.cs
+++ Assets/Scripts/UI/Zoom.cs
@@ -1,49 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 ///<summary>
 /// This class changes the scale of target object by the multiplier  
 /// </summary>
+
 public class Zoom : MonoBehaviour {
 	public GameObject target;
-	public float multiplier = 0.8f;
-    private static int zoomLevel = 0;
-    public int zoomLimit = 5;
-
-    public static int ZoomLevel
-    {
-        get
-        {
-            return zoomLevel;
-        }
-
-        set
-        {
-            zoomLevel = value;
-        }
-    }
+	public float zoomInmultiplier  = 0.8f;
+    public float zoomOutmultiplier = 1.25f;
+    private int currentZoomLevel   = 0;
+    public int zoomLimitMax = 5;
+    public int zoomLimitMin = -5;
 
 
-	public void ZoomTarget() {
-        if (zoomingAllowed()) {
+	public void ZoomTarget(int zoomValue) {
+        if(currentZoomLevel + zoomValue <= zoomLimitMax && currentZoomLevel + zoomValue >= zoomLimitMin) {
+            float multiplier = zoomValue < 0 ? zoomInmultiplier : zoomOutmultiplier;
             Vector3 scale = target.transform.localScale;
             scale = scale * multiplier;
             target.transform.localScale = scale;
+            currentZoomLevel += zoomValue;
         }        
      }
-
-    private bool zoomingAllowed() {
-        if (multiplier < 1 && (ZoomLevel + zoomLimit) > 0)
-        {
-            ZoomLevel--;
-            return true;
-        }
-        if (multiplier > 1 && ZoomLevel < zoomLimit)
-        {
-            ZoomLevel++;
-            return true;
-        }
-        return false;
-    }
     
 }
--- Assets/Tests/Editor/ZoomTest.cs
+++ Assets/Tests/Editor/ZoomTest.cs
@@ -13,35 +13,35 @@ public class ZoomTest {
     [SetUp]
     public void SetUp() {
         target = new GameObject();
-        zoomerObject = new GameObject();        
-        zoomerObject.AddComponent<Zoom>();
-        zoomer = zoomerObject.GetComponent<Zoom>();
-        target.transform.localScale = new Vector3(10f, 10f, 10f);
+        zoomerObject = new GameObject();
+        zoomer = zoomerObject.AddComponent<Zoom>();
+        target.transform.localScale = new Vector3(1f, 1f, 1f);
         zoomer.target = target;
-        zoomer.multiplier = 2f;        
+        zoomer.zoomInmultiplier = 0.5F;
+        zoomer.zoomOutmultiplier = 2.0F;
     }
 
 
 	[Test]
 	public void ZoomTestChangesScaleOfTarget() {		
-		zoomer.ZoomTarget ();        
-		Assert.True (Mathf.Approximately(target.transform.localScale.x, 20f));
+		zoomer.ZoomTarget (1);
+        Assert.True (Mathf.Approximately(target.transform.localScale.x, 2.0f));
+        zoomer.ZoomTarget(-1);
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 1.0f));
     }
 
     [Test]
-    public void NoZoomsPastZoomLimit() { 
-        zoomer.zoomLimit = 1;
-        zoomer.ZoomTarget();
-        zoomer.ZoomTarget();        
-        Assert.True(Mathf.Approximately(target.transform.localScale.x, 20f));
+    public void NoZoomsPastZoomLimit() {
+        for (int i = 0; i < 10; i++)
+        {
+            zoomer.ZoomTarget(1);
+        }
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 32.0F));
+
+        for (int i = 0; i < 10; i++)
+        {
+            zoomer.ZoomTarget(-1);
+        }
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 0.03125F));
     }
-
-    [TearDown]
-    public void CleanUp() {
-        Zoom.ZoomLevel = 0;
-    }
-
-    
-
-
 }
