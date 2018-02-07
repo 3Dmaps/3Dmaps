8494b7c2c99c9ec876bbb1344f6f14a3d5135e25
ollisami <ollisami@cs.helsinki.fi>
2018-02-07 11:05:05 +0200

    Merged master to maps-buttonui

--- Assets/Tests/Editor/ZoomTest.cs
+++ Assets/Tests/Editor/ZoomTest.cs
@@ -4,21 +4,48 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class ZoomTest {
-	Zoom zoomer;
-	[Test]
-	public void ZoomTestChangesScaleOfTarget() {
-		GameObject target = new GameObject();
-		GameObject zoomerObject = new GameObject();
-		zoomerObject.AddComponent<Zoom> ();
-		zoomer = zoomerObject.GetComponent<Zoom> ();
-		target.transform.localScale = new Vector3(10f,10f,10f);
-		zoomer.target = target;
-		zoomer.multiplier = 2f;
-		zoomer.ZoomTarget ();
-		Assert.True (Mathf.Approximately(target.transform.localScale.x, 20f));
+public class ZoomTest
+{
+    Zoom zoomer;
+    GameObject target;
+    GameObject zoomerObject;
 
 
-	}
+    [SetUp]
+    public void SetUp()
+    {
+        target = new GameObject();
+        zoomerObject = new GameObject();
+        zoomer = zoomerObject.AddComponent<Zoom>();
+        target.transform.localScale = new Vector3(1f, 1f, 1f);
+        zoomer.target = target;
+        zoomer.zoomInmultiplier = 0.5F;
+        zoomer.zoomOutmultiplier = 2.0F;
+    }
 
+
+    [Test]
+    public void ZoomTestChangesScaleOfTarget()
+    {
+        zoomer.ZoomTarget(1);
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 2.0f));
+        zoomer.ZoomTarget(-1);
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 1.0f));
+    }
+
+    [Test]
+    public void NoZoomsPastZoomLimit()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            zoomer.ZoomTarget(1);
+        }
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 32.0F));
+
+        for (int i = 0; i < 10; i++)
+        {
+            zoomer.ZoomTarget(-1);
+        }
+        Assert.True(Mathf.Approximately(target.transform.localScale.x, 0.03125F));
+    }
 }
