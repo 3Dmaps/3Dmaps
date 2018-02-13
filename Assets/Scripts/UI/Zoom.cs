fbb13714c34c5c34c1b6ae4dd222f2a0329bdfe4
jurintal1 <jussi.rintala@gmail.com>
2018-02-14 00:31:27 +0200

    zooming with camera's field of view

--- Assets/Scripts/UI/Zoom.cs
+++ Assets/Scripts/UI/Zoom.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 
 ///<summary>
-/// This class changes the scale of target object by the multiplier  
+/// This class changes the main camera's field of vision.  
 /// </summary>
 
 public class Zoom : MonoBehaviour
-{
-    public GameObject target;
+{    
     public float zoomInmultiplier = 0.8f;
     public float zoomOutmultiplier = 1.25f;
     private int currentZoomLevel = 0;
-    public int zoomLimitMax = 5;
-    public int zoomLimitMin = -5;
+    public int zoomLimitMax = 4;
+    public int zoomLimitMin = -4;   
 
 
-    public void ZoomTarget(int zoomValue)
+    public void CameraZoom(int zoomValue)
     {
+        
         if (currentZoomLevel + zoomValue <= zoomLimitMax && currentZoomLevel + zoomValue >= zoomLimitMin)
-        {
-            float multiplier = zoomValue < 0 ? zoomInmultiplier : zoomOutmultiplier;
-            Vector3 scale = target.transform.localScale;
-            scale = scale * multiplier;
-            target.transform.localScale = scale;
+        {            
+            float multiplier = zoomValue < 0 ? zoomOutmultiplier : zoomInmultiplier;            
+            Camera.main.fieldOfView = Camera.main.fieldOfView * multiplier;             
             currentZoomLevel += zoomValue;
         }
     }
