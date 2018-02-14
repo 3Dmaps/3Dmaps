e19b16625521bc6bd59f042c79824658897a18b0
Julius Laitala <julius.laitala@relex.fi>
2018-02-14 13:54:31 +0200

    Add DisplayReadySlice

---
+++ Assets/Scripts/Map Data/DisplayReadySlice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DisplayReadySlice : MapDataSlice {
+
+	public int lod;
+	public DisplayReadySlice(MapDataSlice slice, int lod) : base(slice) {
+		this.lod = lod;
+	}
+
+	private int GetActualLOD() {
+		return lod == 0 ? 1 : lod * 2;
+	}
+
+	public override int GetWidth() {
+
+        int trueValue = base.GetWidth();
+        int remain = trueValue % GetActualLOD();
+        return trueValue - remain + (this.lod > 0 ? 1 : 0);
+
+    }
+
+    public override int GetHeight() {
+
+        int trueValue = base.GetHeight();
+        int remain = trueValue % GetActualLOD();
+        return trueValue - remain + (this.lod > 0 ? 1 : 0);
+
+    }
+	
+}
