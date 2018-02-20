edf70152cabfb6cf934f231d1b5445b195ab95b2
Namirual <lamantyla@gmail.com>
2018-02-20 21:30:29 +0200

    Trail visualization fromlocal map coordinates

---
+++ Assets/Scripts/Trail Visuals/TrailDisplay.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Produces a representation of a trail by generating GameObjects for each node 
+/// in a trail and positioning them correctly relative to the map created from a MapData.
+/// </summary>
+
+public class TrailDisplay : MonoBehaviour {
+
+	public MapData mapData;
+	public GameObject nodeGameObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void DisplayNodes(List<DisplayNode> nodeList) {
+		foreach (DisplayNode node in nodeList) {
+			GenerateNode (node);
+		}
+	}
+
+	public void GenerateNode (DisplayNode node) {
+		int rawX = node.x + ((mapData.GetWidth() - 1) / 2);
+		int rawY = mapData.GetHeight() - 1 - (node.y + ((mapData.GetHeight() - 1) / 2));
+
+		if (!IsWithinBounds(rawX, rawY)) {
+			return;
+		}
+			
+		float height = mapData.GetNormalized (rawX, rawY);
+		Vector3 nodePosition = new Vector3 (((float) node.x * mapData.GetScale()), height, (float) node.y * mapData.GetScale());
+
+		GameObject newNode = Instantiate (nodeGameObject);
+		newNode.transform.position = nodePosition;
+		newNode.transform.SetParent (this.transform);
+	}
+
+	public bool IsWithinBounds(int rawX, int rawY) {
+		if (rawX < 0 || rawX > mapData.GetWidth() - 1) {
+			return false;		
+		}
+
+		if (rawY < 0 || rawY > mapData.GetHeight() - 1) {
+			return false;		
+		}
+
+		return true;
+	}
+}
+
+public class DisplayNode {
+	public int x;
+	public int y;
+
+	public DisplayNode(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ Assets/Tests/Editor/Trail Visuals/TrailDisplayTest.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class TrailDisplayTest {
+
+	public MapData mapdata;
+	public TrailDisplay display;
+
+	[SetUp]
+	public void Setup() {
+		GameObject nodeGameObject = new GameObject ();
+		GameObject trailDisplayObject = new GameObject ();
+
+		display = trailDisplayObject.AddComponent<TrailDisplay> ();
+		display.nodeGameObject = nodeGameObject;
+
+		MapMetadata metadata = MapDataImporter.ReadMetadata("Assets/Resources/testData.txt");
+		display.mapData = MapDataImporter.ReadMapData("Assets/Resources/testData.txt", metadata);
+	}
+
+	[Test]
+	public void TrailDisplayCanCreateNewNodeInCenter() {
+		List<DisplayNode> nodeList = new List<DisplayNode> ();
+		nodeList.Add(new DisplayNode (0, 0));
+
+		display.DisplayNodes (nodeList);
+
+		Assert.True (display.transform.GetChild(0).position.x == 0, "Incorrect x-coordinate!");
+		Assert.True (display.transform.GetChild(0).position.z == 0, "Incorrect y-coordinate!");
+
+		Assert.True (display.transform.childCount == 1, "No node was created!");
+	}
+
+	[Test]
+	public void TrailDisplayCanCreateNewNodesInCorners() {
+		List<DisplayNode> nodeList = new List<DisplayNode> ();
+		nodeList.Add(new DisplayNode (-1, 1));
+		nodeList.Add(new DisplayNode (1, -1));
+		nodeList.Add(new DisplayNode (1, 1));
+		nodeList.Add(new DisplayNode (-1, -1));
+
+		display.DisplayNodes (nodeList);
+
+		Assert.True (display.transform.childCount == 4, "Wrong number of nodes created!");
+	}
+
+	[Test]
+	public void TrailDisplayCannotCreateNodeOutsideMapBounds() {
+		List<DisplayNode> nodeList = new List<DisplayNode> ();
+		nodeList.Add(new DisplayNode (2, 0));
+		nodeList.Add(new DisplayNode (-2, 0));
+		nodeList.Add(new DisplayNode (0, 2));
+		nodeList.Add(new DisplayNode (0, -2));
+
+
+		display.DisplayNodes (nodeList);
+		Assert.True (display.transform.childCount == 0, "Node created out of bounds!");
+	}
+}
