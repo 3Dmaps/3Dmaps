6524af9bf3afd4188f83d154d0180d15718b3757
jurintal1 <jussi.rintala@gmail.com>
2018-03-01 16:42:17 +0200

    introduced nodeGenerationRate variable to control the amount of new nodes created

--- Assets/Scripts/Trail Visuals/TrailGenerator.cs
+++ Assets/Scripts/Trail Visuals/TrailGenerator.cs
@@ -12,8 +12,10 @@ public class TrailGenerator : MonoBehaviour {
 	public MapGenerator mapGenerator;
 	private MapData mapData;
 	private TrailDisplay display;
+	public int nodeGenerationRate = 1; // number of new nodes created between adjacent nodes in data
+	List<DisplayNode> displayNodes;
 
-	// Use this for initialization
+	
 	void Start () {
 		StartCoroutine(StartInit (60));
 	}
@@ -43,33 +45,41 @@ public class TrailGenerator : MonoBehaviour {
 	}
 
 	public List<DisplayNode> TranslateTrail(Trail trail) {
-		List<DisplayNode> displayNodes = new List<DisplayNode> ();
+		displayNodes = new List<DisplayNode> ();
 
 		List<TrailNode> nodes = trail.GetNodeList();
 
 		for (int i = 0; i < nodes.Count - 1 ; i++) {
 			TrailNode node = nodes[i];
 			TrailNode nextNode = nodes[i + 1];			
-			displayNodes.Add (GenerateDisplayNode(node));				
-			displayNodes.Add (GenerateDisplayNodeFromAverage(node, nextNode));;
+			AddDisplayNode(node);				
+			AddDisplayNode(node, nextNode);
 		}
 		TrailNode lastNode = nodes[nodes.Count  - 1];		
-		displayNodes.Add (GenerateDisplayNode(lastNode));
+		AddDisplayNode(lastNode);
 
 		return displayNodes;
 	}
 
-	public DisplayNode GenerateDisplayNode(TrailNode node) {
+	public void AddDisplayNode(TrailNode node) {
 		Vector2 point = mapData.GetMapSpecificCoordinatesFromLatLon (new MapPoint((double) node.GetLon(), (double) node.GetLat ()));
-		return new DisplayNode((int) point.x, (int) point.y);
+		displayNodes.Add(new DisplayNode((int) point.x, (int) point.y));		
 	}
 
-	public DisplayNode GenerateDisplayNodeFromAverage(TrailNode node, TrailNode nextNode) {
-		Vector2 nextPoint = mapData.GetMapSpecificCoordinatesFromLatLon (
-				new MapPoint((double) ((node.GetLon() + nextNode.GetLon()) / 2),
-				(double) ((node.GetLat() + nextNode.GetLat()) / 2))
-		);
-		return new DisplayNode((int) nextPoint.x, (int) nextPoint.y);
+	public void AddDisplayNode(TrailNode node, TrailNode nextNode) {
+		if (nodeGenerationRate < 1) {
+			return;
+		}
+
+		for (int i = 1; i <= this.nodeGenerationRate; i++) {		
+			Vector2 point = mapData.GetMapSpecificCoordinatesFromLatLon (
+					new MapPoint(
+						x: (double) (i * (nextNode.GetLon() - node.GetLon()) / (nodeGenerationRate + 1) + node.GetLon()),
+                        y: (double) (i * (nextNode.GetLat() - node.GetLat()) / (nodeGenerationRate + 1) + node.GetLat())
+					)					
+			);
+			displayNodes.Add(new DisplayNode((int) point.x, (int) point.y));
+		}
 	}
 
 
