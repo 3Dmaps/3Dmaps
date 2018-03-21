479c84cf00a58b5ab6490a032fe0639e5ea6e9c7
Namirual <lamantyla@gmail.com>
2018-03-21 11:36:45 +0200

    Created IconHandler class, fixed Generator prefabs

---
+++ Assets/Scripts/OSM Visuals/IconHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconHandler : MonoBehaviour {
+
+	public List<Icon> iconList; 
+	private Dictionary<string, Icon> icons;
+
+	public IconHandler() {
+		icons = new Dictionary<string, Icon> ();
+	}
+	
+	public Icon SelectIcon(string iconName) {
+		if (icons.ContainsKey(iconName)) {
+			return icons[iconName];
+		} else {
+			return null;
+		}
+	}
+}
