48f17d954ded40c5ad7d6fcf95b1ea02a69a7a54
Op <ollipekka.vaananen@helsinki.fi>
2018-04-04 14:05:59 +0300

    changed name to segmentUtil

--- Assets/Scripts/OSM Visuals/DisplayPoly.cs
+++ Assets/Scripts/OSM Visuals/DisplayPoly.cs
@@ -12,16 +12,6 @@ public class DisplayPoly {
 
 	private List<DisplayNode> boundings = new List<DisplayNode>();
 	private List<Color> colors = new List<Color> ();
-    private int maxX, minX, maxY, minY = 0;
-
-    public void DisplayNodeBoundingBox(List<DisplayNode> dpoints)   {
-
-    minX = dpoints.Max(p=> p.x);
-    minY = dpoints.Min(p => p.y);
-    maxX = dpoints.Max(p => p.x);
-    maxY = dpoints.Max(p => p.y);
-
-}
 
 
 }
