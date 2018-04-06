7261138b0b71fa89f689faa1de193716ce6a7814
Op <ollipekka.vaananen@helsinki.fi>
2018-04-06 14:52:37 +0300

    boundingbox test and started testing segmentutil

---
+++ Assets/Tests/Editor/OSM Visuals/SegmentUtilTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SegmentUtilTest{
+
+
+
+    [Test]
+    public void FindDistanceToSegmentRiverTest() {
+        List<DisplayNode> nodeList = new List<DisplayNode> ();
+        DisplayNode point = new DisplayNode((int) 4, (int) 4);
+        
+
+
+    } 
+}
