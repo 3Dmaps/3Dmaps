92d372fef264ba6e0d48daeeceed84a7ea2aa984
Op <ollipekka.vaananen@helsinki.fi>
2018-04-06 12:37:25 +0300

    bounding box

--- Assets/Scripts/Map Visuals/BoundingBoxUtil.cs
+++ Assets/Scripts/Map Visuals/BoundingBoxUtil.cs
@@ -10,12 +10,20 @@ using System.Linq;
 public static class BoundingBoxUtil{
 
     private static int maxX, minX, maxY, minY = 0;
+     
+    public static List<int> BoundingBox(List<DisplayNode> displaynode)   {
 
-    public static void BoundingBox(List<DisplayNode> displaynode)   {
+        List<int> BoundingBox = new List<int>();
+        minX = displaynode.Max(p=> p.x);
+        BoundingBox.Add(minX);
+        minY = displaynode.Min(p => p.y);
+        BoundingBox.Add(minY);
+        maxX = displaynode.Max(p => p.x);
+        BoundingBox.Add(maxX);
+        maxY = displaynode.Max(p => p.y);       
+        BoundingBox.Add(minY);
 
-    minX = displaynode.Max(p=> p.x);
-    minY = displaynode.Min(p => p.y);
-    maxX = displaynode.Max(p => p.x);
-    maxY = displaynode.Max(p => p.y);    
-}    
+        return BoundingBox;
+
+    }    
 }
