852e1029696e4ca0601d51b02e6878fa9b499ef7
Op <ollipekka.vaananen@helsinki.fi>
2018-04-08 17:03:10 +0300

    test for segmentutil class

--- Assets/Tests/Editor/OSM Visuals/SegmentUtilTest.cs
+++ Assets/Tests/Editor/OSM Visuals/SegmentUtilTest.cs
@@ -7,14 +7,23 @@ using System.Collections.Generic;
 
 public class SegmentUtilTest{
 
-
-
     [Test]
     public void FindDistanceToSegmentRiverTest() {
-        List<DisplayNode> nodeList = new List<DisplayNode> ();
-        DisplayNode point = new DisplayNode((int) 4, (int) 4);
-        
-
-
-    } 
+    DisplayNode dpnode = new DisplayNode(4,4);
+    DisplayNode dpnode2 = new DisplayNode(4, 4);
+    DisplayNode point = new DisplayNode(6,4);
+    Assert.AreEqual(SegmentUtil.FindDistanceToSegmentRiver(point,dpnode,dpnode2),2);        
+    dpnode = new DisplayNode(4,4);
+    dpnode2 = new DisplayNode(6,6);
+    point = new DisplayNode(6,6);
+    Assert.AreEqual(SegmentUtil.FindDistanceToSegmentRiver(point,dpnode,dpnode2),(double) 0.0);
+    dpnode = new DisplayNode(12,12);
+    dpnode2 = new DisplayNode(18,18);
+    point = new DisplayNode(6,16);
+    Assert.AreEqual(SegmentUtil.FindDistanceToSegmentRiver(point,dpnode,dpnode2),7.211102550927978);
+    dpnode = new DisplayNode(24,24);
+    dpnode2 = new DisplayNode(18,18);
+    point = new DisplayNode(6,16);
+    Assert.AreEqual(SegmentUtil.FindDistanceToSegmentRiver(point,dpnode,dpnode2),12.165525060596439);             
+    }
 }
